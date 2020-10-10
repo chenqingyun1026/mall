fb96121bfe1bf27f8f229fd67116812c86be34f3
chenbaba <2736338611@qq.com>
2020-10-10 14:18:01 +0800

    仿写的电商

---
+++ src/test/java/cn/cqy/mall/MallApplicationTests.java
@@ -0,0 +1,24 @@
+package cn.cqy.mall;
+
+import cn.cqy.mall.dao.CategoryMapper;
+import cn.cqy.mall.pojo.Category;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallApplicationTests {
+
+	@Autowired
+	private CategoryMapper categoryMapper;
+
+	@Test
+	public void contextLoads() {
+		Category category = categoryMapper.findById(100001);
+		System.out.println(category);
+	}
+
+}
